7f7533f8f206589dafac00d15077960735f750a3
PArts23 <alonso2405.13@gmail.com>
2023-06-13 11:57:30 -0500

    Se modificaron los inputs de la ventana Nueva Cotizacion

--- src/app/page/coatizacion1/coatizacion1.component.ts
+++ src/app/page/coatizacion1/coatizacion1.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { PlanService } from 'src/app/service/plan.service';
 
 @Component({
   selector: 'app-coatizacion1',
@@ -8,28 +9,39 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
   styleUrls: ['./coatizacion1.component.css']
 })
 
-export class Coatizacion1Component{
+export class Coatizacion1Component {
 
+  tasaEfectiva = false;
+  tasaNominal = false;
   formulario: FormGroup;
-
-  tipo_tasa: string = '';
+  string = '';
   capitalizar: string = '';
   per_gracia: string = '';
   moneda: string = '';
 
-  constructor(public route: ActivatedRoute) { 
+  constructor(public route: ActivatedRoute, private planService: PlanService) {
 
     this.formulario = new FormGroup({
-      tNominal: new FormControl(true),
-      tEfectiva: new FormControl(false),
       Soles: new FormControl(true),
       Dolares: new FormControl(false)
     });
   }
 
-  submitForm() {
+  onCheckboxChange() {
+    if (this.tasaEfectiva && this.tasaNominal!) {
+      this.tasaNominal = true;
+      this.tasaEfectiva = false;
+    }
 
-    // Aquí puedes realizar acciones adicionales con el valor numérico ingresado
+    else
+      if (this.tasaEfectiva! && this.tasaNominal) {
+        this.tasaNominal = false;
+        this.tasaEfectiva = true;
+      }
   }
 
+  Subir_condiciones() {
+
+    //this.planService. ;
+  }
 }
